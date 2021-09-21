82378b5df2c86d2cee62e85aec52a20d3c3aa517
buildpeak <buildpeak@gmail.com>
2021-09-21 23:36:25 +0800

    Add LICENSE.

--- setup.py
+++ setup.py
@@ -1,21 +1,26 @@
 from setuptools import setup, find_packages
+from pathlib import Path
+
+cwd = Path(__file__).resolve().parent
+
+readme = (cwd / "README.md").read_text()
 
 setup(
     name="flask_pydantic_docs",
     version="0.0.1",
-    author="buildpeak",
+    author="Steve Guo",
     author_email="buildpeak@gmail.com",
-    description="",
-    long_description="",
+    description="Add openapi docs to flask app using Flask-Pydantic",
+    long_description=readme,
     long_description_content_type="text/markdown",
     url="https://github.com/buildpeak/flask_pydantic_docs",
     packages=find_packages(exclude=["examples*", "tests*"]),
     package_data={"flask_pydantic_docs": ["templates/*.html"]},
     classifiers=[],
     install_requires=[
-        'Flask',
-        'pydantic',
-        'flask-pydantic',
+        "Flask",
+        "pydantic",
+        "flask-pydantic",
     ],
     zip_safe=False,
     extras_require={},
